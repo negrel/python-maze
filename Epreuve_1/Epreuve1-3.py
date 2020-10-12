4498e1de42a4c3dec44ed04997de83bb6080475d
Romain VIVIEN <romain.vivien@imerir.com>
2020-10-12 12:16:57 +0200

    Update Epreuve1-3.py

--- Epreuve_1/Epreuve1-3.py
+++ Epreuve_1/Epreuve1-3.py
@@ -13,22 +13,24 @@ def CreationChaine(Tab,x,y,ValeurPosition):
 #G : tourner à gauche
 #D : tourner à droite
 #T : aller tout droit (1case)
-    Tab[y][x]=ValeurPosition
-    ChaineCaractere = []
+Tab[y][x]=ValeurPosition
+ChaineCaractere = []
 
-# Si y+1 est plus grande que ValeurPosition
-        if (Tab[y+1][x]>ValeurPosition):
-            CreationChaine (ChaineCaractere.append(D))
+    # Si y-1 est plus grande que ValeurPosition
+    if (Tab[y-1][x]>ValeurPosition):
+        chain = "T"
+        CreationChaine(ChaineCaractere.append(chain))
+        CreationChaine
         
-# Si y-1 est plus grande que ValeurPosition
-        if (Tab[y-1][x]>ValeurPosition):
-        	CreationChaine (ChaineCaractere.append(G))
+    # Si x-1 est plus grande que ValeurPosition
+    if (Tab[y][x-1]>ValeurPosition):
+        chain = "G"
+        CreationChaine(ChaineCaractere.append(chain))
             
-# Si x+1 est plus grande que ValeurPosition
-        if (Tab[y][x+1]>ValeurPosition):
-    	   	CreationChaine (ChaineCaractere.append(T))
-	
+    # Si x+1 est plus grande que ValeurPosition
+    if (Tab[y][x+1]>ValeurPosition):
+        chaine = "D"
+        CreationChaine(ChaineCaractere.append(chain))
 
-	
 
 
