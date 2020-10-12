634b89a8e86c49a80efbb131153f220a1b40feda
eliot.marechal <eliot.marechal@imerir.com>
2020-10-13 00:45:56 +0200

    UPDATE Des commentaires.

--- app.py
+++ app.py
@@ -44,32 +44,42 @@ def cli(grille):
             break
 
 def autoFind(grille):
+    # Définitino des variables de base
     mino_pos = [1, 1]
     mino_orientation = 0
     end = [len(grille[0])-2, len(grille)-2]
     moves = ''
 
+    # Tant que le mino n'est pas sur la case d'arrivée
     while(mino_pos[0] != end[0] or mino_pos[1] != end[1]):
+        # On clear la console et on affiche la labyrinthe
         clear()
         csv1.printcsv(laby, mino_pos)
+        # On bouge le minotaure est on récupère ses nouvelles informations
         mino_pos, mino_orientation, m = algo_gauche.bougerMinotaure(laby, mino_pos, mino_orientation)
+        # On met à jour les mouvements effectués
         moves += m
         time.sleep(0.2)
+    # On réaffiche une dernière fois pour voir le minot aure sur l'arrivée
     csv1.printcsv(laby, mino_pos)
     print("Labyrinthe résolu en "+ str(len(moves)) +" coups.")
     print("Liste des coups :")
     print(moves)
 
-
+# S'il y a moins de deux argument utilisateur, on affiche l'aide et on quitte
+# (Le premier argument est le nom du script)
 if(len(sys.argv) < 3):
     help_app()
     exit()
 
+# On vérifie que le fichier passé en argument existe, sinon on quitte
 filename = sys.argv[2]
 if not os.path.exists(filename):
     print("Le fichier '"+ filename +"' n'existe pas.")
     exit()
 
+# S'il existe, on regarde ce que l'utilisateur veut faire et on l'envoit
+# sur la bonne fonction
 laby = csv1.readcsv(filename)
 if(sys.argv[1] == 'play'):
     cli(laby)
