c88e06cf629e4688feb0caf770324713f26e9a14
Alexandre Negrel <negrel.dev@protonmail.com>
2020-10-12 17:08:13 +0200

    mise à jour du test de csv1

--- csv1_test.py
+++ csv1_test.py
@@ -9,4 +9,4 @@ VIDE = '  '
 VERT = '\033[92m'
 ROUGE = '\033[91m'
 BLEU = '\033[94m'
-csv1.printcsv(matrice, VERT, ROUGE, (4,1), BLEU, MUR, VIDE)
+csv1.printcsv(matrice, (4,1), VERT, ROUGE, BLEU, MUR, VIDE)
