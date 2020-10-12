eaa717cce4aa30c06929e1c15037f20cf79bf0b1
eliot.marechal <eliot.marechal@imerir.com>
2020-10-12 14:06:56 +0200

    ADD Commentaires

--- csv1.py
+++ csv1.py
@@ -2,10 +2,14 @@ def readcsv(filename):
     matrice = []
 
     with open(filename,'r') as fic:
+        # On parcourt chaque ligne du CSV
         for ligne in fic:
+            # Pour chaque ligne, on split le string de la cellule qu'on convertit
+            # en int dans la liste m_ligne, qu'on append dans matrice
             m_ligne = [int(cell) for cell in str.split(ligne, ',')]
             matrice.append(m_ligne)
             
+    # On retire la première ligne, qui correspond aux dimensions du labyrinthe
     matrice = matrice[1:]
     return matrice
 
