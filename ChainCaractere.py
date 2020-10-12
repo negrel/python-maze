cc5d9ecd0bec4a619c963d2824595122b5192fde
Romain VIVIEN <romain.vivien@imerir.com>
2020-10-12 15:09:25 +0200

    Modification du code et commentaire

--- ChainCaractere.py
+++ ChainCaractere.py
@@ -4,41 +4,46 @@
 Created on Mon Oct 12 11:42:08 2020
 @author: vivien
 """
-#     T
-#    y-1
-#G x-1    x+1 D
-#    y+1
-#     T
+#        T
+#       y-1
+#G  x-1    x+1 D
+#       y+1
+#        T
 
-def CreationChaine(Tab,x,y,ValeurPosition):
+def CreationChaine(Tab,x,y,Position): # La fontion parcous le tableau et ajoute les caractères.
 #G : tourner à gauche
 #D : tourner à droite
 #T : aller tout droit (1case)
-    Tab[y][x]=ValeurPosition
-    ChaineCaractere = []
 
-    # Si y-1 est plus grande que ValeurPosition
-    if (Tab[y-1][x]>ValeurPosition):
-        ValeurPosition = [y-1][x]
+    Position = Tab[y][x]
+    ChaineCaractere = []
+    
+    # Si y-1 est plus grande que Position
+    if (int(Tab[y-1][x]>Position)):
+        CreationChaine(Position == [y-1][x])
         chain = "T"
         CreationChaine(ChaineCaractere.append(chain))
         CreationChaine()
         
-    # Si y+1 est plus grande que ValeurPosition
-    if (Tab[y+1][x]>ValeurPosition):
-        ValeurPosition = [y+1][x]
+    # Si y+1 est plus grande que Position
+    if (int(Tab[y+1][x]>Position)):
+        CreationChaine(Position == [y+1][x])
         chain = "T"
         CreationChaine(ChaineCaractere.append(chain))
         CreationChaine()
-        
-    # Si x-1 est plus grande que ValeurPosition
-    if (Tab[y][x-1]>ValeurPosition):
+
+    # Si x-1 est plus grande que Position
+    if (int(Tab[y][x-1]>Position)):
+        CreationChaine(Position == [y][x-1])
         chain = "G"
         CreationChaine(ChaineCaractere.append(chain))
         CreationChaine()
             
-    # Si x+1 est plus grande que ValeurPosition
-    if (Tab[y][x+1]>ValeurPosition):
+    # Si x+1 est plus grande que Position
+    if (int(Tab[y][x+1]>Position)):
+        CreationChaine(Position == [y][x+1])
         chain = "D"
         CreationChaine(ChaineCaractere.append(chain))
         CreationChaine()
+        
+    return Tab
