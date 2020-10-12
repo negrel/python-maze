f5c3560106d5294790f4dca4a704e3b50cfd2468
eliot.marechal <eliot.marechal@imerir.com>
2020-10-12 14:00:56 +0200

    FIX Lignes vides; changement de noms de variables; ajout de variables

--- csv1.py
+++ csv1.py
@@ -1,34 +1,26 @@
-
 def readcsv(filename):
-
-    
     matrice = []
-    
+
     with open(filename,'r') as fic:
         for ligne in fic:
-            x = str.split(ligne, ',')
-            y = [int(z) for z in x ]
-            matrice.append(y)
+            m_ligne = [int(cell) for cell in str.split(ligne, ',')]
+            matrice.append(m_ligne)
             
-    
     matrice = matrice[1:]
-    
     return matrice
-matrice = readcsv('Laby exemple.csv')
+
+matrice = readcsv('./example/example1.csv')
 print(matrice)
 
-
-
 def printcsv(matrice, mur, vide):
-    
-    
     for ligne in matrice:
         s = ''
         for cellule in ligne:
             s += mur if cellule == -1 else vide
-            
         print(s)
 
-printcsv(matrice, '\u2588' + '\u2588', '  ')
+MUR = '\u2588\u2588'
+VIDE = '  '
+printcsv(matrice, MUR, VIDE)
 
 
