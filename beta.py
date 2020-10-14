599a9f74efa3c582b6334f08893f986a3723a8e0
Alexandre Negrel <negrel.dev@protonmail.com>
2020-10-14 21:11:35 +0200

    mise à jour de update.py

--- beta.py
+++ beta.py
@@ -9,22 +9,75 @@ import resolveur
 
 from components import *
 
+# Couleurs
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Taille standard
+BTN_WIDTH = 248
+BTN_HEIGHT = 56
 
 app = Application("Maze", 1920, 1080)
-width, height = app.get_size()
-CENTER = (width / 2, height / 2)
+# app.debug(True)
 
-app.debug(True)
-
+window_width, window_height = app.get_size()
+CENTER = (window_width / 2, window_height / 2)
 
 # On utilise un image comme fond de fenêtre
 image = Image(*CENTER, os.path.join(".", "assets", "background.jpg"))
 image.scale(*app.get_size())
-app.appendElement(image)
 
-btn = Button(*CENTER, 300, 100, "Quitter", lambda event: app.quit(), background=BLACK)
+# Liste des pages
+menu = None
+play = None
+solve = None
 
-app.appendElement(btn)
+# Menu
+menu = Page("menu")
+menu.appendElement(image)
+app.push_page(menu)
+
+menu.appendElement(
+    Button(*CENTER,
+           BTN_WIDTH,
+           BTN_HEIGHT,
+           "Jouer",
+           lambda event: app.push_page(play),
+           background=WHITE,
+           color=BLACK))
+menu.appendElement(
+    Button(CENTER[0],
+           CENTER[1] + 92,
+           BTN_WIDTH,
+           BTN_HEIGHT,
+           "Résoudre",
+           lambda event: print("Résoudre"),
+           background=WHITE,
+           color=BLACK))
+
+# Bouton pour quitter l'application
+btn_quitter = Button(CENTER[0] + (BTN_WIDTH + 10),
+                     (window_height - window_height / 10),
+                     BTN_WIDTH,
+                     BTN_HEIGHT,
+                     "Quitter",
+                     lambda event: app.quit(),
+                     background=BLACK)
+menu.appendElement(btn_quitter)
+
+btn_retour = Button(CENTER[0] - (BTN_WIDTH + 10),
+                    (window_height - window_height / 10),
+                    BTN_WIDTH,
+                    BTN_HEIGHT,
+                    "Retour",
+                    lambda event: app.pop_page(),
+                    background=BLACK)
+menu.appendElement(btn_retour)
+
+# Play menu
+play = Page("play")
+play.appendElement(image)
+play.appendElement(btn_quitter)
+play.appendElement(btn_retour)
 
 app.start()
